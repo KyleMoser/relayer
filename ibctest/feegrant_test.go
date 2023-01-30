68d9db1e352e7a28d479b32f5bed7f9efbf77e65
kyle <moser.kyle@gmail.com>
2023-01-30 14:19:35 -0500

    Making sure relayer and lens configs are synced in test cases

--- ibctest/feegrant_test.go
+++ ibctest/feegrant_test.go
@@ -1,25 +1,27 @@
-package ibctest_test
+package ibctest
 
 import (
 	"context"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
-	relayeribctest "github.com/cosmos/relayer/v2/ibctest"
 	"github.com/cosmos/relayer/v2/relayer"
 	"github.com/cosmos/relayer/v2/relayer/processor"
-	"github.com/strangelove-ventures/ibctest/v5"
+	ibctestv5 "github.com/strangelove-ventures/ibctest/v5"
 	"github.com/strangelove-ventures/ibctest/v5/ibc"
 	"github.com/strangelove-ventures/ibctest/v5/test"
 	"github.com/strangelove-ventures/ibctest/v5/testreporter"
+	"github.com/strangelove-ventures/lens/client"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 	"golang.org/x/sync/errgroup"
 )
 
-// TestScenarioFeegrant relay multiple TXs using relayer with feegrants enabled and verify TXs are feegranted.
-// We query each account's bank balances to check whether fees were taken from each relevant grantee/granter.
-func TestScenarioFeegrant(t *testing.T) {
+// TestScenarioFeegrantBasic Feegrant on a single chain
+// Run this test with e.g. go test -timeout 300s -run ^TestScenarioFeegrantBasic$ github.com/cosmos/relayer/v2/ibctest
+func TestScenarioFeegrantBasic(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 
@@ -27,7 +29,7 @@ func TestScenarioFeegrant(t *testing.T) {
 	nf := 0
 
 	// Chain Factory
-	cf := ibctest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctest.ChainSpec{
+	cf := ibctestv5.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctestv5.ChainSpec{
 		{Name: "gaia", Version: "v7.0.3", NumValidators: &nv, NumFullNodes: &nf},
 		{Name: "osmosis", Version: "v11.0.1", NumValidators: &nv, NumFullNodes: &nf},
 	})
@@ -37,18 +39,18 @@ func TestScenarioFeegrant(t *testing.T) {
 	gaia, osmosis := chains[0], chains[1]
 
 	// Relayer Factory to construct relayer
-	r := relayeribctest.NewRelayerFactory(relayeribctest.RelayerConfig{
+	r := NewRelayerFactory(RelayerConfig{
 		Processor:           relayer.ProcessorEvents,
 		InitialBlockHistory: 100,
 	}).Build(t, nil, "")
 
 	// Prep Interchain
 	const ibcPath = "gaia-osmosis"
-	ic := ibctest.NewInterchain().
+	ic := ibctestv5.NewInterchain().
 		AddChain(gaia).
 		AddChain(osmosis).
 		AddRelayer(r, "relayer").
-		AddLink(ibctest.InterchainLink{
+		AddLink(ibctestv5.InterchainLink{
 			Chain1:  gaia,
 			Chain2:  osmosis,
 			Relayer: r,
@@ -59,10 +61,10 @@ func TestScenarioFeegrant(t *testing.T) {
 	rep := testreporter.NewNopReporter()
 	eRep := rep.RelayerExecReporter(t)
 
-	client, network := ibctest.DockerSetup(t)
+	client, network := ibctestv5.DockerSetup(t)
 
 	// Build interchain
-	require.NoError(t, ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
+	require.NoError(t, ic.Build(ctx, eRep, ibctestv5.InterchainBuildOptions{
 		TestName:  t.Name(),
 		Client:    client,
 		NetworkID: network,
@@ -83,16 +85,44 @@ func TestScenarioFeegrant(t *testing.T) {
 
 	// Create and Fund User Wallets
 	fundAmount := int64(10_000_000)
-	users := ibctest.GetAndFundTestUsers(t, ctx, "default", int64(fundAmount), gaia, osmosis)
+	// Tiny amount of funding, not even enough to pay for a single TX fee (that is the whole point - the GRANTER should be paying the fee)
+	granteeFundAmount := int64(10)
 
-	gaiaUser, osmosisUser := users[0], users[1]
+	//Mnemonic from the juno repo test user
+	granterMnemonic := "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose"
+	granterUsers := GetAndFundTestUsers(t, ctx, "default", granterMnemonic, int64(fundAmount), gaia, osmosis)
+	fundRecipientUsers := GetAndFundTestUsers(t, ctx, "recipient", "", int64(fundAmount), gaia, osmosis)
+	gaiaGranterUser, osmosisGranterUser := granterUsers[0], granterUsers[1]
+	gaiaRecipient, osmosisRecipient := fundRecipientUsers[0], fundRecipientUsers[1]
+
+	usersGrantees := GetAndFundTestUsers(t, ctx, "grantee1", "", int64(granteeFundAmount), gaia)
+	gaiaGrantee := usersGrantees[0]
 
 	r.StartRelayer(ctx, eRep, ibcPath)
+	logger := zaptest.NewLogger(t)
+
+	//TODO: why is this necessary? How do we link chain config to RELAYER config?
+	if err := r.RestoreKey(ctx,
+		eRep,
+		gaia.Config().ChainID, gaiaGranterUser.KeyName,
+		granterMnemonic,
+	); err != nil {
+		t.Fatalf("failed to restore key to relayer for chain %s: %s", gaia.Config().ChainID, err.Error())
+	}
+
+	localRelayer := r.(*Relayer)
+	res := localRelayer.sys().Run(logger, "chains", "configure", "feegrant", "basicallowance", gaia.Config().Name, gaiaGranterUser.KeyName, "--grantees", "10", "--overwrite-grantees")
+	if res.Err != nil {
+		t.Fatalf("failed to rly config feegrants: %v", res.Err)
+	}
 
 	// Send Transaction
-	amountToSend := int64(1_000_000)
-	gaiaDstAddress := gaiaUser.Bech32Address(osmosis.Config().Bech32Prefix)
-	osmosisDstAddress := osmosisUser.Bech32Address(gaia.Config().Bech32Prefix)
+	amountToSend := int64(1_000)
+	gaiaDstAddress := gaiaRecipient.Bech32Address(osmosis.Config().Bech32Prefix)
+	osmosisDstAddress := osmosisRecipient.Bech32Address(gaia.Config().Bech32Prefix)
+
+	gaiaGranteeAddr := gaiaGrantee.Bech32Address(gaia.Config().Bech32Prefix)
+	gaiaGranterAddr := gaiaGranterUser.Bech32Address(gaia.Config().Bech32Prefix)
 
 	gaiaHeight, err := gaia.Height(ctx)
 	require.NoError(t, err)
@@ -102,7 +132,7 @@ func TestScenarioFeegrant(t *testing.T) {
 
 	var eg errgroup.Group
 	eg.Go(func() error {
-		tx, err := gaia.SendIBCTransfer(ctx, gaiaChannel.ChannelID, gaiaUser.KeyName, ibc.WalletAmount{
+		tx, err := gaia.SendIBCTransfer(ctx, gaiaChannel.ChannelID, gaiaGranterUser.KeyName, ibc.WalletAmount{
 			Address: gaiaDstAddress,
 			Denom:   gaia.Config().Denom,
 			Amount:  amountToSend,
@@ -120,7 +150,7 @@ func TestScenarioFeegrant(t *testing.T) {
 	})
 
 	eg.Go(func() error {
-		tx, err := osmosis.SendIBCTransfer(ctx, osmosisChannel.ChannelID, osmosisUser.KeyName, ibc.WalletAmount{
+		tx, err := osmosis.SendIBCTransfer(ctx, osmosisChannel.ChannelID, osmosisGranterUser.KeyName, ibc.WalletAmount{
 			Address: osmosisDstAddress,
 			Denom:   osmosis.Config().Denom,
 			Amount:  amountToSend,
@@ -154,4 +184,43 @@ func TestScenarioFeegrant(t *testing.T) {
 	osmosisIBCBalance, err := gaia.GetBalance(ctx, osmosisDstAddress, osmosisIbcDenom)
 	require.NoError(t, err)
 	require.Equal(t, amountToSend, osmosisIBCBalance)
+
+	// Test grantee still has exact amount expected
+	gaiaGranteeIBCBalance, err := gaia.GetBalance(ctx, gaiaGranteeAddr, gaiaIbcDenom)
+	require.NoError(t, err)
+	require.Equal(t, granteeFundAmount, gaiaGranteeIBCBalance)
+
+	// Test granter has less than they started with, meaning fees came from their account
+	gaiaGranterIBCBalance, err := gaia.GetBalance(ctx, gaiaGranterAddr, gaiaIbcDenom)
+	require.NoError(t, err)
+	require.Less(t, gaiaGranterIBCBalance, fundAmount)
+}
+
+func fundAccount(t *testing.T, ctx context.Context, cc *client.ChainClient, keyNameReceiveFunds string, keyNameSendFunds string, amountCoin string, gas uint64) string {
+	fromAddr, err := cc.GetKeyAddressForKey(keyNameSendFunds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	toAddr, err := cc.GetKeyAddressForKey(keyNameReceiveFunds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	coins, err := sdk.ParseCoinsNormalized(amountCoin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := &banktypes.MsgSend{
+		FromAddress: cc.MustEncodeAccAddr(fromAddr),
+		ToAddress:   cc.MustEncodeAccAddr(toAddr),
+		Amount:      coins,
+	}
+
+	res, err := cc.SubmitTxAwaitResponse(ctx, []sdk.Msg{req}, "", gas, keyNameSendFunds)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return res.TxResponse.TxHash
 }
---
+++ ibctest/test_user.go
@@ -0,0 +1,97 @@
+package ibctest
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/strangelove-ventures/ibctest/v5/ibc"
+	"github.com/strangelove-ventures/ibctest/v5/test"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+const FaucetAccountKeyName = "faucet"
+
+// GetAndFundTestUsers generates and funds chain users with the native chain denom.
+// The caller should wait for some blocks to complete before the funds will be accessible.
+func GetAndFundTestUsers(
+	t *testing.T,
+	ctx context.Context,
+	keyName string,
+	mnemonic string,
+	amount int64,
+	chains ...ibc.Chain,
+) []*ibc.Wallet {
+	users := make([]*ibc.Wallet, len(chains))
+	var eg errgroup.Group
+	for i, chain := range chains {
+		i := i
+		chain := chain
+		eg.Go(func() error {
+			user, err := GetAndFundTestUserWithMnemonic(ctx, keyName, mnemonic, amount, chain)
+			if err != nil {
+				return err
+			}
+			users[i] = user
+			return nil
+		})
+	}
+	require.NoError(t, eg.Wait())
+
+	// TODO(nix 05-17-2022): Map with generics once using go 1.18
+	chainHeights := make([]test.ChainHeighter, len(chains))
+	for i := range chains {
+		chainHeights[i] = chains[i]
+	}
+	return users
+}
+
+// generateUserWallet creates a new user wallet with the given key name on the given chain.
+// a user is recovered if a mnemonic is specified.
+func generateUserWallet(ctx context.Context, keyName, mnemonic string, chain ibc.Chain) (*ibc.Wallet, error) {
+	if mnemonic != "" {
+		if err := chain.RecoverKey(ctx, keyName, mnemonic); err != nil {
+			return nil, fmt.Errorf("failed to recover key on source chain: %w", err)
+		}
+	} else {
+		if err := chain.CreateKey(ctx, keyName); err != nil {
+			return nil, fmt.Errorf("failed to create key on source chain: %w", err)
+		}
+	}
+	userAccountAddressBytes, err := chain.GetAddress(ctx, keyName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get source user account address: %w", err)
+	}
+	user := ibc.Wallet{
+		KeyName: keyName,
+		Address: string(userAccountAddressBytes),
+	}
+	return &user, nil
+}
+
+// GetAndFundTestUserWithMnemonic restores a user using the given mnemonic
+// and funds it with the native chain denom.
+// The caller should wait for some blocks to complete before the funds will be accessible.
+func GetAndFundTestUserWithMnemonic(
+	ctx context.Context,
+	keyName, mnemonic string,
+	amount int64,
+	chain ibc.Chain,
+) (*ibc.Wallet, error) {
+	chainCfg := chain.Config()
+	user, err := generateUserWallet(ctx, keyName, mnemonic, chain)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get source user wallet: %w", err)
+	}
+
+	err = chain.SendFunds(ctx, FaucetAccountKeyName, ibc.WalletAmount{
+		Address: user.Bech32Address(chainCfg.Bech32Prefix),
+		Amount:  amount,
+		Denom:   chainCfg.Denom,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get funds from faucet: %w", err)
+	}
+	return user, nil
+}
